688d6051f1d4ae4ea3ecb2cd24aba68d290da508
matt989253 <matthieu.burguburu@gmail.com>
2020-05-24 18:24:58 +0200

    Step 2b: Install express, ignore node_modules, answer simple content with js

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,4 +1,17 @@
 var Chance = require('chance');
 var chance = new Chance();
 
-console.log("Bonjour " + chance.name());
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send("Hello, RES!");
+});
+
+app.get('/test', function(req, res) {
+	res.send("Hello, RES! Your test worked!");
+});
+
+app.listen(3000, function() {
+	console.log('Accepting HTTP requests on port 3000.');
+});
